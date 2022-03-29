9f58d20becf9fa840e4bb66037aebf999b370a8c
SpearJin <ckdwls2525@gmail.com>
2022-03-29 18:18:45 +0900

    morgan & nodemon 설정

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const orderRoutes = require('./routes/orders');
@@ -7,6 +8,22 @@ const productRoutes = require('./routes/products');
 
 app.use('/orders', orderRoutes);
 app.use('/products', productRoutes);
+
+app.use(morgan('short'));
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      msg: error.message,
+    },
+  });
+});
 
 const PORT = 3000;
 
