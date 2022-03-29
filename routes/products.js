82214dfdb4aba0f4776e90c44ca83e6af3d9ab57
SpearJin <ckdwls2525@gmail.com>
2022-03-29 19:17:37 +0900

    수정, 삭제, 상품 불러오기

--- routes/products.js
+++ routes/products.js
@@ -47,17 +47,70 @@ router.post('/', (req, res) => {
 });
 
 // 데이터 수정
-router.put('/', (req, res) => {
-  res.status(200).json({
-    msg: 'product data 데이터 수정',
-  });
+router.put('/:productId', (req, res) => {
+  const id = req.params.productId;
+  const { name, price } = req.body;
+
+  productModel
+    .updateOne(
+      { _id: id },
+      {
+        name,
+        price,
+      }
+    )
+    .then((result) => {
+      res.status(200).json({
+        msg: 'Product 수정 성공',
+        updateProduct: result,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        erorr,
+      });
+    });
 });
 
 // 데이터 삭제
-router.delete('/', (req, res) => {
-  res.status(200).json({
-    msg: 'product data 데이터 삭제',
-  });
+router.delete('/:productId', (req, res) => {
+  const id = req.params.productId;
+
+  productModel
+    .remove({ _id: id })
+    .then((result) => {
+      res.status(200).json({
+        msg: 'Prodcut 삭제 성공',
+        deleteProduct: result,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error,
+      });
+    });
+});
+
+// 특정 상품 불러오기
+router.get('/:productId', (req, res) => {
+  const id = req.params.productId;
+
+  productModel
+    .findById({ _id: id })
+    .then((result) => {
+      res.status(200).json({
+        msg: '상품 불러오기',
+        getProduct: result,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        erorr,
+      });
+    });
 });
 
 module.exports = router;
