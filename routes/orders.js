7e2585f32ee32f972a4ca43b86f5a530159cfb7b
SpearJin <ckdwls2525@gmail.com>
2022-03-30 14:04:39 +0900

    order 수정 삭제 검색

--- routes/orders.js
+++ routes/orders.js
@@ -8,9 +8,20 @@ const productModel = require('../models/product');
 router.get('/', (req, res) => {
   orderModel
     .find()
-    .then((result) => {
+    .then((docs) => {
       res.status(200).json({
-        result,
+        count: docs.length,
+        order: docs.map((doc) => {
+          return {
+            _id: doc._id,
+            productId: doc.product,
+            quantity: doc.quantity,
+            request: {
+              type: 'GET',
+              url: 'http://localhost:3000/orders/' + doc._id,
+            },
+          };
+        }),
       });
     })
     .catch((error) => {
@@ -39,7 +50,6 @@ router.post('/', (req, res) => {
       return order.save();
     })
     .then((result) => {
-      console.log(result);
       res.status(200).json({
         msg: 'Order stored',
         createOrder: {
@@ -62,17 +72,72 @@ router.post('/', (req, res) => {
 });
 
 // 데이터 수정
-router.put('/', (req, res) => {
-  res.status(200).json({
-    msg: 'order data 데이터 수정',
-  });
+router.put('/:orderId', (req, res) => {
+  const id = req.params.orderId;
+  const { quantity } = req.body.quantity;
+  orderModel
+    .updateOne({ _id: id }, { quantity })
+    .then((result) => {
+      res.status(200).json({
+        msg: 'Order 수정 성공',
+        request: {
+          type: 'GET',
+          url: 'http://localhost:3000/orders/' + result._id,
+        },
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error,
+      });
+    });
 });
 
 // 데이터 삭제
-router.delete('/', (req, res) => {
-  res.status(200).json({
-    msg: 'order data 데이터 삭제',
-  });
+router.delete('/:orderId', (req, res) => {
+  const id = req.params.orderId;
+  orderModel
+    .deleteOne({ _id: id })
+    .then((result) => {
+      res.status(200).json({
+        msg: 'Order 삭제 성공',
+        request: {
+          type: 'GET',
+          url: 'http://localhost:3000/orders/',
+        },
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error,
+      });
+    });
+});
+
+// 데이터 검색
+router.get('/:orderId', (req, res) => {
+  const id = req.params.orderId;
+
+  orderModel
+    .findOne({ _id: id })
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        msg: '상품 불러오기',
+        request: {
+          type: 'GET',
+          url: 'http://localhost:3000/orders',
+        },
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error,
+      });
+    });
 });
 
 module.exports = router;
